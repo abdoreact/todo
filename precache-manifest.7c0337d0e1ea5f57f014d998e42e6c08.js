22de392161ae9419f3f7c3aa4dd98626e19bd448
abdoabdoabdoabdoabdo <abdonaggar77@gmail.com>
2020-08-10 13:00:04 +0200

    Updates

---
+++ precache-manifest.7c0337d0e1ea5f57f014d998e42e6c08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9421545532907d4eadbbea358e8e277e",
+    "url": "/github.io/todo/index.html"
+  },
+  {
+    "revision": "b5b1f202b6a9a737039b",
+    "url": "/github.io/todo/static/js/2.b5763d74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/todo/static/js/2.b5763d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b59651f4d7e1b96013",
+    "url": "/github.io/todo/static/js/main.7854adf9.chunk.js"
+  },
+  {
+    "revision": "ad0ff1d2869dc02ef53e",
+    "url": "/github.io/todo/static/js/runtime-main.3a9ddb4c.js"
+  }
+]);
